cbea905c6d55741b1431aca623d23382b728581e
AlexGIchi <ichimerasu1996@gmail.com>
2025-12-02 14:48:51 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Seleccionamos un elemento aleatorio de cada array
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+
+  // Colocamos la excusa en el HTML
+  document.getElementById("excuse").innerHTML = excuse;
+}
